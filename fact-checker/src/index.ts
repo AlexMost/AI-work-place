ed2f71b8f13b95b9d0f5211a2295fbc63e1044f9
a.mostovenko <a.mostovenko@smartweb.com.ua>
2026-04-06 23:32:24 +0200

    added support for ukrainian wiki

--- fact-checker/src/index.ts
+++ fact-checker/src/index.ts
@@ -1,9 +1,17 @@
 import 'dotenv/config';
 import { checkFact } from './factCheck';
+import { ChatOpenAI } from '@langchain/openai';
 
-const main = async () => {
-  const result = await checkFact("Ukraine territory is bigger than France");
+const main = async (fact: string) => {
+  console.log('----- gpt-5.4');
+  const llm = new ChatOpenAI({ model: 'gpt-5.4' });
+  const resultSimpleModel = await llm.invoke(fact);
+  console.log(resultSimpleModel.content);
+  console.log('----- wiki fact checker ----');
+  const result = await checkFact(fact);
+
   console.log(result);
+
 };
 
-main();
+main('Іран заблокував Ормузьку протоку');
